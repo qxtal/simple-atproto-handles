39e3e897c81b289e263c5f89770c8654da464cce
qxtal <79723895+qxtal@users.noreply.github.com>
2025-08-31 01:08:56 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ import { type DocumentHead, type RequestHandler } from "@builder.io/qwik-city";
 export const onGet: RequestHandler = async ({ status, headers, send }) => {
   status(503);
   headers.set('Content-Type', 'text/plain');
-  send(new Response('Service Temporarily Unavailable'));
+  send(new Response('This AT Protocol handle service is currently not open for new registrations at this moment.'));
 };
 
 export default component$(() => {
